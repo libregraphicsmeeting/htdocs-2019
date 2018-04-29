1343cbba1a8d9c4c189a90ec4956204ed58d1f3b
ale rimoldi <ale@graphicslab.org>
2018-04-29 19:04:52 +0200

    now that the hooks uses https, it should work

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
   <body>
 	<h1>Libre Graphics Meeting 2019</h1>
 	<p>This is the future page for the LGM 2019.</p>
+	<p>The [sources for this sites](https://github.com/libregraphicsmeeting/htdocs-2019) are on Github.</p>
   </body>
 </html>
