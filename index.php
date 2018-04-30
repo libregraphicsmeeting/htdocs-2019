82c57fd3d56174ee6d058e6e5ef3a8878c7cfc95
ale rimoldi <ale@graphicslab.org>
2018-04-30 07:24:10 +0200

    -...

--- index.php
+++ index.php
@@ -8,6 +8,5 @@
 	<h1>Libre Graphics Meeting 2019</h1>
 	<p>This is the future page for the LGM 2019.</p>
 	<p><a href="https://github.com/libregraphicsmeeting/htdocs-2019">The sources for this site</a> are on Github.</p>
-    <p>...</p>
   </body>
 </html>
